18f9aead7afc5ad90a807cc3a398d6047ab46f76
pushpasree <mpushpasree@gmail.com>
2020-01-23 04:12:09 -0800

    assignment 1

---
+++ app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private firstNumber: number=null;
+  private secondNumber:number=null;
+  private result:number=null;
+  
+  
+  public add():number{
+    this.result=this.firstNumber+this.secondNumber;
+    return this.result;
+  }
+  public sub():number{
+    this.result=this.firstNumber-this.secondNumber;
+    return this.result;
+  }
+  public mul():number{
+    this.result=this.firstNumber*this.secondNumber;
+    return this.result;
+  }
+  public div():number{
+    this.result=this.firstNumber/this.secondNumber;
+    return this.result;
+  }
+}
